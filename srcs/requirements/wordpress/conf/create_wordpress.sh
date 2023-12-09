ba154dcf4355d752002c67c33d3ed8220880ad14
RGuilhem <g.raux@icloud.com>
2023-12-09 10:35:21 +0100

    Corrected values read from env files

--- srcs/requirements/wordpress/conf/create_wordpress.sh
+++ srcs/requirements/wordpress/conf/create_wordpress.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Start BASH SCRIPT" > my_bashLog.txt
 env > /tmp/test
-while ! mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $WORDPRESS_DB_NAME ; do
+while ! mysql -h $MYSQL_HOST -u $MYSQL_USER -p $MYSQL_USER_PASS $WORDPRESS_DB_NAME ; do
 	sleep 3
 	echo "Error mysql" >> my_bashLog.txt
 done
@@ -25,7 +25,7 @@ else
 	ls -l html > content.txt
 	cd /var/www/html || exit
 	wp core download --allow-root > dl_log.txt 2> dl_error_log.txt
-	wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root > config_log.txt 2>config_error_log.txt
+	wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_USER_PASS --dbhost=$MYSQL_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root > config_log.txt 2>config_error_log.txt
 	wp core install --url=$DOMAIN_NAME --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_DB_ADMIN --admin_password=$WORDPRESS_DB_ADMIN_PWD --admin_email=$WORDPRESS_DB_ADMIN_MAIL --skip-email --allow-root >core_install_log.txt 2>core_install_error_log.txt
 	wp user create $WORDPRESS_DB_USER $WORDPRESS_DB_MAIL --role=author --user_pass=$WORDPRESS_DB_PASSWORD --allow-root >usercreate_log.txt 2>usercreate_error_log.txt
 	
