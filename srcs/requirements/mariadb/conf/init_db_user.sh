6fdb537d1a985b5cfb99f3c768f0fa437e522b6b
RGuilhem <g.raux@icloud.com>
2023-12-11 08:33:51 +0100

    Lauching of maridb looooks better

--- srcs/requirements/mariadb/conf/init_db_user.sh
+++ srcs/requirements/mariadb/conf/init_db_user.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 cat << EOF > /tmp/init_db_user.sql
-CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+CREATE DATABASE IF NOT EXISTS $MYSQL_DB;
 CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASS';
-GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
+GRANT ALL ON $MYSQL_DB.* TO '$MYSQL_USER'@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASS';
 FLUSH PRIVILEGES;
 EOF
 
-cat /tmp/init_db_user.sql
-#mysql -h localhost < /tmp/init_db_user.sql
+/usr/bin/mariadbd --user=mysql & sleep 2
+/usr/bin/mariadb -h localhost < /tmp/init_db_user.sql
+
+pkill mariadbd
+/usr/bin/mariadbd --user=mysql
